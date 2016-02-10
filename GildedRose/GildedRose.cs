ab8a3c9a036857fe90b7473c57ea482c69e6de9e
Dom Edwards <dedwards@qualtrax.com>
2016-02-10 09:04:18 -0500

    initial commit

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (QualityCanDecrease(Items[i]))
+                {
+                    DecrementQuality(Items[i], 1);// for normal items
+                }
+                else // for aged brie and backstage passes
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        IncrementQuality(Items[i], 1);
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11 && Items[i].Quality < 50)
+                            {
+                                IncrementQuality(Items[i], 1);
+                            }
+
+                            if (Items[i].SellIn < 6 && Items[i].Quality < 50)
+                            {
+                                IncrementQuality(Items[i], 1);
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                DecrementQuality(Items[i], 1);
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality; // quality becomes  0 if backstage pass
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            IncrementQuality(Items[i], 1);//incrememnt quality of aged brie
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool QualityCanDecrease(Item item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
+                return true;
+
+            return false;
+        }
+
+        private void IncrementQuality(Item item, Int32 multiplier)
+        {
+            item.Quality = item.Quality + multiplier;
+        }
+
+        private void DecrementQuality(Item item, Int32 multiplier)
+        {
+            item.Quality = item.Quality - multiplier;
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+
+}
---
+++ UnitTestProject1/GildedRoseTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GildedRoseKata;
+using System.Collections.Generic;
+
+namespace GildedRoseTests
+{
+    [TestClass]
+    public class GildedRoseTest
+    {
+        private Item normalItem;
+        private Item agedBrie;
+        private Item backstagePass;
+        private Item Sulfuras;
+        private List<Item> Items;
+        private GildedRose gildedRose;
+        [TestInitialize]
+        public void Initialize()
+        {
+            normalItem = new Item();
+            normalItem.Name = "Gloves";
+            normalItem.SellIn = 5;
+            normalItem.Quality = 20;
+            agedBrie = new Item();
+            agedBrie.Name = "Aged Brie";
+            agedBrie.SellIn = 5;
+            agedBrie.Quality = 5;
+            backstagePass = new Item();
+            backstagePass.Name = "Backstage passes to a TAFKAL80ETC concert";
+            backstagePass.Quality = 30;
+            backstagePass.SellIn = 15;
+            Sulfuras = new Item();
+            Sulfuras.Name = "Sulfuras, Hand of Ragnaros";
+            Sulfuras.Quality = 80;
+            Items = new List<Item>();
+            Items.Add(normalItem);
+            Items.Add(agedBrie);
+            Items.Add(backstagePass);
+            Items.Add(Sulfuras);
+            gildedRose = new GildedRose(Items);
+        }
+
+        [TestMethod]
+        public void TestNormalItemQualityDecay()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(19, normalItem.Quality);
+        }
+
+        [TestMethod]
+        public void TestNormalItemQualityDecayPastSellIn()
+        {
+            UpdateQualityXTimes(6);
+            Assert.AreEqual(13, normalItem.Quality);
+        }
+
+        [TestMethod]
+        public void TestNormalItemSellInDecay()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(4, normalItem.SellIn);
+        }
+
+        [TestMethod]
+        public void TestNormalItemSellInZero()
+        {
+            UpdateQualityXTimes(5);
+            Assert.AreEqual(0, normalItem.SellIn);
+        }
+
+        [TestMethod]
+        public void TestNormalItemZeroOrGreaterQuality()
+        {
+            UpdateQualityXTimes(21);
+            Assert.AreEqual(0, normalItem.Quality);
+        }
+
+        [TestMethod]
+        public void TestAgedBrieQualityIncrease()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(6, agedBrie.Quality);
+        }
+
+        [TestMethod]
+        public void TestAgedBrieQualityIncreaseAfterSellIn()
+        {
+            UpdateQualityXTimes(6);
+            Assert.AreEqual(12, agedBrie.Quality);
+        }
+
+        [TestMethod]
+        public void TestAgedBrieQualityCap()
+        {
+            UpdateQualityXTimes(50);
+            Assert.AreEqual(50, agedBrie.Quality);
+        }
+
+        [TestMethod]
+        public void TestAgedBrieSellInDecay()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(4, agedBrie.SellIn);
+        }
+
+        [TestMethod]
+        public void TestBackStagePassSellInDecay()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(14, backstagePass.SellIn);
+        }
+
+        [TestMethod]
+        public void TestBackstagePassQualityIncrease()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(31, backstagePass.Quality);
+        }
+
+        [TestMethod]
+        public void TestBackstagePassQualityIncrease10Daysleft()
+        {
+            UpdateQualityXTimes(6);
+            Assert.AreEqual(37, backstagePass.Quality);
+        }
+
+        [TestMethod]
+        public void TestBackstagePassQualityIncrease5Daysleft()
+        {
+            UpdateQualityXTimes(11);
+            Assert.AreEqual(48, backstagePass.Quality);
+        }
+
+        [TestMethod]
+        public void TestBackstagePassQualityCap()
+        {
+            UpdateQualityXTimes(13);
+            Assert.AreEqual(50, backstagePass.Quality);
+        }
+
+        [TestMethod]
+        public void TestBackstagePassQualityReset()
+        {
+            UpdateQualityXTimes(17);
+            Assert.AreEqual(0, backstagePass.Quality);
+        }
+
+        [TestMethod]
+        public void TestSulfurasNoQualityDecay()
+        {
+            UpdateQualityXTimes(1);
+            Assert.AreEqual(80, Sulfuras.Quality);
+        }
+
+        private void UpdateQualityXTimes(Int32 days)
+        {
+            for (var i = 0; i < days; i++)
+                gildedRose.UpdateQuality();
+        }
+
+    }
+}
